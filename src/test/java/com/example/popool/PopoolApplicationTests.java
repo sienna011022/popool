ed050e6c5d302baca854d3746ecfce020c0038bf
홍혁준 <jjj8337721@naver.com>
2022-04-20 19:23:39 +0900

    Initial commit

---
+++ src/test/java/com/example/popool/PopoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
